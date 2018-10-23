dd1003a5b9aaa44a080ead157fc7f9ada2218f26
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2018-10-23 10:35:53 -0300

    test: add example dog queries and mutations

--- api/src/resolvers/Mutation.js
+++ api/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutation = {};
+global.dogs = global.dogs || []
+
+const Mutation = {
+  createDog(parent, args, ctx, info) {
+    const newDog = { name: args.name }
+    global.dogs.push(newDog)
+    return newDog
+  },
+};
 
 module.exports = Mutation;
