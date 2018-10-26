7afada0f5ff6e98160b9fb049402066fa551f585
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2018-10-26 11:13:38 -0300

    app: add items page

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,9 +1,10 @@
 import { Fragment } from 'react'
 import Link from 'next/link'
+import Items from '../components/Items'
 
 const Home = () => (
   <Fragment>
-    <p>Home</p>
+    <Items />
   </Fragment>
 )
 
