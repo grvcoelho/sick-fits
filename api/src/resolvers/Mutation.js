d8066ad28949d21462154d9494326222b994cd15
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2018-11-20 18:07:44 -0200

    api: add reset password

--- api/src/resolvers/Mutation.js
+++ api/src/resolvers/Mutation.js
@@ -1,5 +1,9 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { randomBytes } = require('crypto')
+const { promisify } = require('util')
+
+const randomBytesPromisified = promisify(randomBytes)
 
 const defaultCookieOptions = {
   httpOnly: true,
@@ -85,6 +89,61 @@ const Mutation = {
     ctx.response.clearCookie('token', defaultCookieOptions)
 
     return { message: 'Goodbye!' }
+  },
+
+  async requestResetToken(parent, args, ctx, info) {
+    const { email } = args
+
+    const user = await ctx.db.query.user({ where: { email } })
+
+    if (!user) throw new Error(`No such user found for email ${email}`)
+
+    const resetToken = (await randomBytesPromisified(20)).toString('hex')
+    const resetTokenExpiry = Date.now() + 3600000
+
+    const res = await ctx.db.mutation.updateUser({
+      where: { email },
+      data: { resetToken, resetTokenExpiry },
+    })
+
+    console.log(res)
+
+    return { message: 'Sent!' }
+  },
+
+  async resetPassword(parent, args, ctx, info) {
+    const { resetToken, password, confirmPassword } = args
+
+    if (password !== confirmPassword) {
+      throw new Error('Your passwords don\'t match')
+    }
+
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000,
+      },
+    })
+
+    if (!user) {
+      throw new Error('This token is either invalid or expired')
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password: hashedPassword,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    })
+
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET)
+    ctx.response.cookie('token', defaultCookieOptions)
+
+    return updatedUser
   },
 };
 
